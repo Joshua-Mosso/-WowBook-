cfde069e3bd6d9aa7c510be6963e1fb81b7b9f00
Joshua <joshuakingphotography23@gmail.com>
2020-06-01 15:57:12 +0100

    Criação do projeto
    initial commit

---
+++ WOWBOOK/app/src/main/java/com/example/wowbook/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wowbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Direção para a pagina de signUp
+        Button signButton = (Button) findViewById(R.id.sign_up);
+        Intent intent = new Intent(this, SignUp.class);
+
+        signButton.setOnClickListener(this, SignUp.class);
+    }
+
+}
